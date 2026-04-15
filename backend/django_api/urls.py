45eb7b4bc0b9ce09ab0ec417b159bb611e0ec87b
FransiM. <ayelefransi@gmail.com>
2026-04-15 15:40:32 +0300

    new

--- backend/django_api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('analyze-alert', views.analyze_alert, name='analyze_alert'),
-    path('health', views.health_check, name='health_check'),
-]
